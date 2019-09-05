7f17ea45ff06a27ee8005b95a4d212ecb311cb51
Marinella Sigurdardottir <marinellasigurdardottir@Marinellas-MacBook-Pro-2.local>
2019-09-05 10:35:42 +0000

    Sequence

--- max_int.py
+++ max_int.py
@@ -1,16 +1,16 @@
 num_int = int(input("Input a number: "))   
 
-largest = 0
+max_int = 0
 count = 0
 
 #While integer is positive
-while num_int > 0:
+while num_int >= 0:
     #Find maximum integer
     if num_int > max_int:
-        num_int = max_int
+        max_int = num_int
 
     count += 1
     num_int = int(input("Input a number: "))
 
-if count > 0:
+if count >= 0:
     print("The maximum is", max_int)  
